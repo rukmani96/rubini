637df0768d7fdef12ef4a676757307ac669cb23c
rukmani96 <30343784+rukmani96@users.noreply.github.com>
2017-07-23 11:38:31 +0530

    Create hij.c

---
+++ hij.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+printf("enter any alphabet\n:");
+scanf(%c",c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+{
+printf("given alphabet is a vowel\n",c);
+}
+else
+{
+printf("given alphabet is a vowel\n",c);
+}
+getch();
+}
+
