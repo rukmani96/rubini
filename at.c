8ba99a9deb30303f4a8097386343b05241c0b23b
rukmani96 <30343784+rukmani96@users.noreply.github.com>
2017-07-23 12:30:54 +0530

    Update at.c

--- at.c
+++ at.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
-#include<<conio.h>
 int main()
 {
 int a,b,c,n;
 printf("enter three numbers:");
+  scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+  printf("a is the greatest number:\n");
+}
+  if(b>a&&b>c)
+  {
+    printf("b is greatest number:\n");
+  }
+  if(c>a&&c>b)
+  {
+    printf("c is greatest number:\n");
+  }
+  return 0;
+}
